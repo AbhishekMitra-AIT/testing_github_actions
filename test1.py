baa05a6b6df1a14ef64836ed481976b532e97144
Abhishek Mitra <abhishekmitra91@gmail.com>
2025-11-20 00:09:30 +0530

    testing github actions
    testing char limit

--- test1.py
+++ test1.py
@@ -1,7 +1,5 @@
 
-# print("Hello World this is from test1.py for checking if the 
-#       github actions are working or not, this line contains more 
-#       than 160 charcters so that the line length check can be 
-#       tested.")
+print("Hello World this is from test1.py for checking if the github actions are working or not, " \
+"this line contains more than 160 charcters so that the line length check can be tested.")
 
-print("this line of the code has less than 160 characters")
+# print("this line of the code has less than 160 characters")
